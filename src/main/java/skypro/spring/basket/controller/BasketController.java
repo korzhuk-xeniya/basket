64a7881aba4ef20647d27a448401b9c42667cb3c
Ксения <korzhuk.xeniya@yandex.ru>
2023-09-18 20:30:14 +0300

    task done

--- src/main/java/skypro/spring/basket/controller/BasketController.java
+++ src/main/java/skypro/spring/basket/controller/BasketController.java
@@ -1,4 +1,29 @@
 package skypro.spring.basket.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import skypro.spring.basket.service.BasketService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/order")
 public class BasketController {
+    private final BasketService basketService;
+
+    public BasketController(BasketService basketService) {
+        this.basketService = basketService;
+    }
+
+    @GetMapping("/add")
+    public String add(@RequestParam List<Long> items) {
+        basketService.add(items);
+        return "Товары добавлены";
+    }
+
+    @GetMapping("/get")
+    public List<Long> get() {
+        return basketService.get();
+    }
 }
--- src/main/java/skypro/spring/basket/service/BasketService.java
+++ src/main/java/skypro/spring/basket/service/BasketService.java
@@ -1,4 +1,10 @@
 package skypro.spring.basket.service;
 
+import java.util.List;
+
 public interface BasketService {
+
+    void add(List<Long> items);
+
+    List<Long> get();
 }
